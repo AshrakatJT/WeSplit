90e7bd0852f04e071ccae837ed6a333def45829e
Ashrakat <ocheyjt@icloud.com>
2022-09-15 15:16:49 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ashrakat Sherif on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
